27cfb99bfd06211fc4f5d31efb167669eb594a2f
themanwhocodesmad <marcusstowell@gmail.com>
2023-08-20 11:59:04 +0200

    login view fix 1

--- webapp/views/LoginView.py
+++ webapp/views/LoginView.py
@@ -1,16 +1,16 @@
 from datetime import datetime, timedelta
 
-from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
-
-# views.py
-
-from django.shortcuts import redirect
 from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.checks import messages
+from django.shortcuts import redirect
+from django.shortcuts import render
 from knox.models import AuthToken
 
-from webapp.forms import LoginForm
+
+# views.py
 
 
 def logout_view(request):
@@ -18,28 +18,67 @@ def logout_view(request):
     return redirect('login')
 
 
+#
+# def login_view(request):
+#     if request.method == "POST":
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             # Extract username and password from the validated form
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#
+#             # Authenticate the user
+#             user = authenticate(request, username=username, password=password)
+#
+#             if user is not None:
+#                 # User is authenticated, so log them in
+#                 login(request, user)
+#
+#                 # Generate Knox token
+#                 instance, token = AuthToken.objects.create(user)
+#
+#                 # Prepare the response
+#                 response = JsonResponse({"message": "Logged in successfully!"})
+#
+#                 # Set the token in an HttpOnly cookie
+#                 expiration = datetime.utcnow() + timedelta(hours=10)  # adjust as needed
+#                 response.set_cookie(
+#                     key='token',
+#                     value=token,
+#                     expires=expiration,
+#                     httponly=True,
+#                     secure=True  # Ensure cookie is only sent over HTTPS (omit if working locally without HTTPS)
+#                 )
+#                 return redirect('dashboard')
+#             else:
+#                 # Authentication failed
+#                 return render(request, 'utility_pages/login.html', {'error': 'Invalid credentials'})
+#     else:
+#         # If it's a GET request or any other method, show the form
+#         form = LoginForm()
+#
+#     return render(request, 'utility_pages/login.html', )
+
+
+from django.urls import reverse
+
+
 def login_view(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # Extract username and password from the validated form
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-
-            # Authenticate the user
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
-                # User is authenticated, so log them in
                 login(request, user)
 
                 # Generate Knox token
                 instance, token = AuthToken.objects.create(user)
 
-                # Prepare the response
-                response = JsonResponse({"message": "Logged in successfully!"})
-
                 # Set the token in an HttpOnly cookie
+                response = redirect(reverse('dashboard'))  # assuming 'dashboard' is the name of your URL pattern
                 expiration = datetime.utcnow() + timedelta(hours=10)  # adjust as needed
                 response.set_cookie(
                     key='token',
@@ -48,16 +87,16 @@ def login_view(request):
                     httponly=True,
                     secure=True  # Ensure cookie is only sent over HTTPS (omit if working locally without HTTPS)
                 )
-                return redirect('dashboard')
+                return response
             else:
-                # Authentication failed
-                return render(request, 'utility_pages/login.html', {'error': 'Invalid credentials'})
-    else:
-        # If it's a GET request or any other method, show the form
-        form = LoginForm()
+                messages.error(request, 'Invalid username or password.')
+        else:
+            messages.error(request, 'Invalid username or password.')
 
-    return render(request, 'utility_pages/login.html', )
+    form = AuthenticationForm()
+    return render(request, 'utility_pages/login.html', {'form': form})
 
+#
 # def login_view(request):
 #     if request.method == 'POST':
 #         username = request.POST.get('username')
@@ -73,4 +112,3 @@ def login_view(request):
 #             return render(request, 'utility_pages/login.html', {'error': 'Invalid credentials'})
 #
 #     return render(request, 'utility_pages/login.html')
-
